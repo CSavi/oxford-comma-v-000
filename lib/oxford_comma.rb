1e183723a6cca186e11832ea2d0e7eb7c6c897e8
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 15:33:37 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(string_array)
      return string_array.join
   elsif string_array.length == 2 
      return string_array.join(" and ")
-   elsif string_array.length == 3 
-     return string_array.join(" ,  , and")
+   elsif string_array.length > 2 
+     return string_array.join(" ,  and")
   end 
 end 

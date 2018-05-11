7f34e4dbbdd5f60063650c2637c58316c561b4a6
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 15:51:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(string_array)
   elsif string_array.length == 2 
      return string_array.join(" and ")
    elsif string_array.length > 2 
-     string.array.to_sentence
+     string_array.to_sentence
       
        
    

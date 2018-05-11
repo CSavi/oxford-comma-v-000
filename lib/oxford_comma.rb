819848504834162c2c977e6be5c38041570c52d4
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 16:11:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,5 +5,6 @@ def oxford_comma(string_array)
      return string_array.join(" and ")
   elsif string_array.length > 2 
      return string_array[-1].prepend("and") 
+     return string_array.join(", ")
   end
 end   

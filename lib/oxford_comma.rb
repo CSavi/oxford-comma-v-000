25700c7d0999a45383302027a6319eb681c5a721
Carrie Savinell <carrielane89@gmail.com>
2018-05-11 16:02:58 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(string_array = nil)
-  return string_array.to_s if string_array.nil? || string_array.length <= 1 
+  return string_array.join if string_array.nil? || string_array.length <= 1 
   array[0..-2].join(", ") + " and " + array[-1]
   end
 end 
